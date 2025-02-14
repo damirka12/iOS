52c303e336fabf573cef795d7fd03da0ca947cc5
damirka12 <145275371+damirka12@users.noreply.github.com>
2025-02-14 21:31:49 +0500

    Add files via upload

---
+++ dzka1/dzka1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dzka1
+//
+//  Created by Дамир Алимжан on 14.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dzka1/dzka1Tests/dzka1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  dzka1Tests.swift
+//  dzka1Tests
+//
+//  Created by Дамир Алимжан on 14.02.2025.
+//
+
+import Testing
+@testable import dzka1
+
+struct dzka1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
